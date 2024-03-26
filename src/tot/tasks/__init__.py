c9cb0893a770528bfd982481e0fe28ec69c892cd
Mounir Lahlouh <mounirlahlouh@gmail.com>
2024-03-26 11:28:06 +0100

    Init py ajout task bias

--- src/tot/tasks/__init__.py
+++ src/tot/tasks/__init__.py
@@ -8,5 +8,8 @@ def get_task(name):
     elif name == 'crosswords':
         from tot.tasks.crosswords import MiniCrosswordsTask
         return MiniCrosswordsTask()
+    elif name == 'biasToxicity':
+        from tot.tasks.biasToxicity import BiasToxicityTask
+        return BiasToxicityTask()
     else:
         raise NotImplementedError
