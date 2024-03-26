8fc4ca2a8ab091e7ef359758fd94b514abb4200a
Mounir Lahlouh <mounirlahlouh@gmail.com>
2024-03-26 14:20:28 +0100

    biasToxicity script created

---
+++ scripts/biasToxicity/bfs.sh
@@ -0,0 +1,13 @@
+python run.py \
+    --task biasToxicity \
+    --task_start_index 0 \
+    --task_end_index 10 \
+    --method_generate sample \
+    --method_evaluate vote \
+    --method_select greedy \
+    --n_generate_sample 5 \
+    --n_evaluate_sample 5 \
+    --n_select_sample 1 \
+    --prompt_sample cot \
+    --temperature 1.0 \
+    ${@}
---
+++ scripts/biasToxicity/standard_sampling.sh
@@ -0,0 +1,9 @@
+python run.py \
+    --task biasToxicity \
+    --task_start_index 0 \
+    --task_end_index 10 \
+    --naive_run \
+    --prompt_sample standard \
+    --n_generate_sample 10 \
+    --temperature 1.0 \
+    ${@}
